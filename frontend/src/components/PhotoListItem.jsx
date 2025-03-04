8f614da6beab729416bd2fd5523884f13f86e1ca
AnthonyA <alicea.9a@gmail.com>
2025-03-04 11:46:27 -0700

    feat: Add class styling to PhotoListItem component from imported scss file

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,12 +5,15 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   const {id, location, urls, user} = props.sampleDataForPhotoListItem;
   return (
-    <div>
-      <p>id={id}</p>
-      <img src={urls.regular} alt="Regular image" />
-      <img src={user.profile} alt="Profile Image" />
-      <p>{user.name}</p>
-      <p>{location.city}, {location.country}</p>
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={urls.regular} alt="Regular image" />
+      <div className="photo-list__user-details">
+        <img className='photo-list__user-profile' src={user.profile} alt="Profile Image" />
+        <div className="photo-list__user-info">
+          {user.name}
+          <div className="photo-list__user-location">{location.city}, {location.country}</div>
+        </div>
+      </div>
     </div>
   )
 };
