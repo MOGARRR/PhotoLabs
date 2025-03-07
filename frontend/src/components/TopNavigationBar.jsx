1eadec3332d62ebac98b5ba1c9836d4964eec3ed
AnthonyA <alicea.9a@gmail.com>
2025-03-07 08:30:12 -0700

    refactor: Change FavIcon to FaveBadge component and us proper function in TopNavigationBar

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,6 +1,6 @@
 import '../styles/TopNavigationBar.scss'
 import TopicList from './TopicList';
-import FavIcon from './FavIcon';
+import FavBadge from './FavBadge'
 
 const TopNavigation = (props) => {
   const {topics,notifaction} = props;
@@ -8,7 +8,7 @@ const TopNavigation = (props) => {
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={topics}/>
-      <FavIcon displayAlert={notifaction} selected={true}/>
+      <FavBadge isFavPhotoExist={notifaction}/>
     </div>
   )
 };
