b1bf0d70d2e67ee2ac3babe23765982939c1c840
AnthonyA <alicea.9a@gmail.com>
2025-03-13 11:55:42 -0600

    feat: Add svg icons and animation for lightBlub icon in TopNavigation component

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -2,6 +2,8 @@ import "../styles/TopNavigationBar.scss";
 import TopicList from "./TopicList";
 import FavBadge from "./FavBadge";
 import { useEffect } from "react";
+import lightBlub from "../assets/light-bulb.svg"
+import lightBlubDark from "../assets/light-bulb-dark.svg"
 
 
 const TopNavigation = (props) => {
@@ -17,7 +19,13 @@ const TopNavigation = (props) => {
   return (
     <div className="top-nav-bar ">
       <span className={`top-nav-bar__logo ${dark}`}>PhotoLabs</span>
-      <i className={`icon`} onClick={toggleDarkMode}>💡</i>
+
+      <img
+      className={`icon`}
+      src={dark === "" ? lightBlub : lightBlubDark}
+      alt="Light blub symbol"
+      onClick={toggleDarkMode} />
+
       <TopicList
       topics={topics}
       setTopicId={setTopicId}
